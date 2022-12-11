f261a66a52567a1707b28f03231455a23ef9d711
shahin <shahin.rmz@tutanota.com>
2022-12-11 22:00:01 +0100

    adds py file

---
+++ selen.py
@@ -0,0 +1,11 @@
+##
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+##
+
+
+##
+driver=webdriver.Chrome("/usr/bin/chromedriver")
+driver.get('https://google.com')
+##
